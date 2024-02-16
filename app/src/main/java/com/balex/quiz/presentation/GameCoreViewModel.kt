970ee25fe7c7d19c4067d4c052b76256490c26e2
balex <balexvicx@gmail.com>
2024-02-16 14:41:36 +0100

    try glide with bitmap

--- app/src/main/java/com/balex/quiz/presentation/GameCoreViewModel.kt
+++ app/src/main/java/com/balex/quiz/presentation/GameCoreViewModel.kt
@@ -1,17 +1,31 @@
 package com.balex.quiz.presentation
 
 import android.app.Application
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.balex.quiz.data.QuizRepositoryImpl
+import com.balex.quiz.data.api.ApiFactory
 import com.balex.quiz.domain.entity.Country
 import com.balex.quiz.domain.entity.Level
 import com.balex.quiz.domain.entity.Question
 import com.balex.quiz.domain.usecases.GenerateQuestionUseCase
 import com.balex.quiz.domain.usecases.GetGameSettingsUseCase
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.util.Collections
+import java.util.stream.Collectors
+
 
 class GameCoreViewModel(
     private val application: Application,
@@ -25,6 +39,7 @@ class GameCoreViewModel(
     var countriesFullList: List<Country> = Collections.emptyList()
     var countriesNotUsedInQuestion: List<Country> = Collections.emptyList()
 
+    var bitmapNextQuestion: Bitmap? = null
 
     private val _question = MutableLiveData<Question>()
     val question: LiveData<Question>
@@ -34,14 +49,44 @@ class GameCoreViewModel(
 
     var imageName = MutableLiveData<String>()
 
+    private val compositeDisposable = CompositeDisposable()
 
-    fun generateQuestion(numQuestion: Int) : Question {
-        val question = generateQuestion(level, numQuestion, countriesFullList, countriesNotUsedInQuestion)
+    fun generateQuestion(numQuestion: Int): Question {
+        val question =
+            generateQuestion(level, numQuestion, countriesFullList, countriesNotUsedInQuestion)
         val country =
             countriesFullList.stream().filter { it.countryName == question.countryName }.findFirst()
                 .get()
         imageName.value = country.imageName
         return generateQuestion(level, numQuestion, countriesFullList, countriesNotUsedInQuestion)
+    }
+
+    fun loadImageAsBitmapFromBackend(imageFileName: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+
+
+            Glide.with(application)
+                .asBitmap()
+                .load("${ApiFactory.BASE_URL_STATIC_IMAGES}/${imageFileName}")
+                .into(object : CustomTarget<Bitmap>() {
+                    override fun onResourceReady(
+                        resource: Bitmap,
+                        transition: Transition<in Bitmap>?
+                    ) {
+                        //imageView.setImageBitmap(resource)
+                        bitmapNextQuestion = resource
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {
+                        // this is called when imageView is cleared on lifecycle call or for
+                        // some other reason.
+                        // if you are referencing the bitmap somewhere else too other than this imageView
+                        // clear it here as you can no longer have the bitmap
+                    }
+                })
+
+        }
+
     }
 
     fun pr() {
@@ -50,5 +95,9 @@ class GameCoreViewModel(
         Log.d("ddd", generateQuestion(1).toString())
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
 
 }
--- app/src/main/java/com/balex/quiz/presentation/fragments/GameCoreFragment.kt
+++ app/src/main/java/com/balex/quiz/presentation/fragments/GameCoreFragment.kt
@@ -1,12 +1,15 @@
 package com.balex.quiz.presentation.fragments
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import com.balex.quiz.R
 import com.balex.quiz.data.api.ApiFactory
 import com.balex.quiz.databinding.CoreTestBinding
 import com.balex.quiz.presentation.App
@@ -16,6 +19,10 @@ import com.balex.quiz.presentation.MainViewModel
 import com.balex.quiz.presentation.MainViewModelFactory
 import com.bumptech.glide.Glide
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class GameCoreFragment : Fragment() {
     private val TAG = "GameCoreFragment"
@@ -29,7 +36,7 @@ class GameCoreFragment : Fragment() {
         ViewModelProvider(this, gameViewModelFactory)[GameCoreViewModel::class.java]
     }
 
-
+    var bitmapNextQuestion: Bitmap? = null
 
     private var userName = ""
     private var _binding: CoreTestBinding? = null
@@ -65,6 +72,7 @@ class GameCoreFragment : Fragment() {
 
     private fun initViewValues() {
         userName = App.loadUserNameFromPrefsCapitalized(requireActivity().application)
+        //binding.ivImageCapital.setImageResource(R.drawable.eye)
     }
 
 private fun observeViewModel() {
@@ -72,6 +80,29 @@ private fun observeViewModel() {
         Glide.with(this)
             .load("${ApiFactory.BASE_URL_STATIC_IMAGES}/${it}")
             .into(binding.ivImageCapital)
+
+
+
+
+        val str = "Uzbekistan-Tashkent.jpg"
+
+        CoroutineScope(Dispatchers.IO).launch {
+            bitmapNextQuestion = Glide.with(requireActivity())
+                .asBitmap()
+                .load("${ApiFactory.BASE_URL_STATIC_IMAGES}/${str}")
+                .submit().get()
+
+        }
+
+        val handler = Handler()
+        handler.postDelayed({
+            Glide.with(requireActivity())
+                .load(bitmapNextQuestion)
+                .into(binding.ivImageCapital)
+        }, 5000)
+
+
+
         //Picasso.get().load("${ApiFactory.BASE_URL}/${ApiFactory.BACKEND_STATIC_IMAGES_PREFIX}/".into(ivImageCapital)
 
     }
