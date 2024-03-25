33700716ef05fde6539929113e0a39720531cb3d
balex <balexvicx@gmail.com>
2024-03-25 14:33:24 +0100

    invoke syntaxis

--- app/src/main/java/com/balex/quiz/domain/usecases/GetGameSettingsUseCase.kt
+++ app/src/main/java/com/balex/quiz/domain/usecases/GetGameSettingsUseCase.kt
@@ -1,11 +1,14 @@
 package com.balex.quiz.domain.usecases
 
-import com.balex.quiz.domain.entity.GameSettings
+//import com.balex.quiz.domain.entity.GameSettings
 import com.balex.quiz.domain.entity.Level
 import com.balex.quiz.domain.repository.QuizRepository
 
 class GetGameSettingsUseCase(private val quizRepository: QuizRepository) {
-    operator fun invoke(level: Level): GameSettings {
-        return quizRepository.getGameSettings(level)
-    }
+//    operator fun invoke(level: Level): GameSettings {
+//        return quizRepository.getGameSettings(level)
+//    }
+
+    operator fun invoke(level: Level) = quizRepository.getGameSettings(level)
+
 }
